1eef0ea1043b48593fc2029fc7420efea236f821
monirul8888 <monirul429588@gmail.com>
2026-02-06 20:59:50 +0600

    Update Email

--- pydantic_test.py
+++ pydantic_test.py
@@ -1,9 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl
 from typing import List, Dict, Optional
 
 class patient(BaseModel):
     name: str
     age: int
+    email: EmailStr
+    fb_url: AnyUrl
     weight: float
     married: bool=False
     allergies: Optional[List[str]]=None
@@ -13,6 +15,8 @@ def insert(patient: patient):
 
     print(patient.name)
     print(patient.age)
+    print(patient.email)
+    print(patient.fb_url)
     print(patient.weight)
     print(patient.married)
     print(patient.allergies)
@@ -20,7 +24,7 @@ def insert(patient: patient):
     print("Data Inserted Successfully")
 
 
-patient_info={"name":"Monirul Islam", "age": 24, "weight": 62.5,  "contact": {"Phone":"07142958888"}}
+patient_info={"name":"Monirul Islam", "age": 24, "email":"monirul@gmail.com", "fb_url":"https://www.facebook.com/moniirrul","weight": 62.5, "contact": {"Phone":"07142958888"}}
 
 patient_1=patient(**patient_info)
 
