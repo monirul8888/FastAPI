cb92f5b768956776c40b371d61551b388cf89502
monirul8888 <monirul429588@gmail.com>
2026-02-07 14:30:27 +0600

    Update

--- pydantic_test.py
+++ pydantic_test.py
@@ -1,13 +1,14 @@
-from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator, model_validator, computed_field
 from typing import List, Dict, Optional, Annotated
 
-class patient(BaseModel):
+class Patient(BaseModel):
     name: Annotated[str, Field(max_length=50, title="Name of The Patient ", description="Name of Patient Less than 50 Char", examples=["Monirul", "Akib"])]
 
     age: int= Field (gt=0, lt=120)
     email: EmailStr
     fb_url: AnyUrl
     weight: float= Field(gt=0, lt=150)
+    height: float
     married: Annotated[bool, Field(default=None, description="Is that Patient Married or Not")]
     allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
     contact: Dict[str, str]
@@ -28,13 +29,29 @@ class patient(BaseModel):
     @classmethod
     def name_validator(cls, value):
         return value.upper()
+    
+    @model_validator(mode="after")
+    def validate_emergency_contact(cls, model):
+        if model.age>60 and "emergency" not in model.contact:
+            raise ValueError("Patient older than 60 must have and Emergency Contact")
+        return model
+    
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi=round((self.weight)/(self.height**2),2)
+
+        return bmi
 
 
 
-def insert(patient: patient):
+def insert(patient: Patient):
 
     print(f"Name: {patient.name}")
     print(f"Age: {patient.age}")
+    print(f"Height: {patient.height}")
+    print(f"Weight: {patient.weight}")
+    print(f"BMI: {patient.bmi}")
     print(f"Email: {patient.email}")
     print(f"Any URL: {patient.fb_url}")
     print(f"Weight: {patient.weight}")
@@ -44,9 +61,9 @@ def insert(patient: patient):
     print("Data Inserted Successfully")
 
 
-patient_info={"name":"Monirul Islam", "age": 24, "email":"monirul@hdfc.com", "fb_url":"https://www.facebook.com/moniirrul","weight": 62.5, "contact": {"Phone":"07142958888"}}
+patient_info={"name":"Monirul Islam", "age": 65, "weight":62, "height": 1.65, "email":"monirul@hdfc.com", "fb_url":"https://www.facebook.com/moniirrul","weight": 62.5, "contact": {"Phone":"07142958888", "emergency":"0174295888"}}
 
-patient_1=patient(**patient_info)
+patient_1=Patient(**patient_info)
 
 insert(patient_1)
 
