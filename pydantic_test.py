82ef60d64cf9e73f975f946b727db6cdccc6c2e8
monirul8888 <monirul429588@gmail.com>
2026-02-07 12:36:12 +0600

    Update

--- pydantic_test.py
+++ pydantic_test.py
@@ -1,26 +1,27 @@
-from pydantic import BaseModel, EmailStr, AnyUrl
-from typing import List, Dict, Optional
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
 
 class patient(BaseModel):
-    name: str
-    age: int
+    name: Annotated[str, Field(max_length=50, title="Name of The Patient ", description="Name of Patient Less than 50 Char", examples=["Monirul", "Akib"])]
+
+    age: int= Field (gt=0, lt=120)
     email: EmailStr
     fb_url: AnyUrl
-    weight: float
-    married: bool=False
-    allergies: Optional[List[str]]=None
+    weight: float= Field(gt=0, lt=150)
+    married: Annotated[bool, Field(default=None, description="Is that Patient Married or Not")]
+    allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
     contact: Dict[str, str]
 
 def insert(patient: patient):
 
-    print(patient.name)
-    print(patient.age)
-    print(patient.email)
-    print(patient.fb_url)
-    print(patient.weight)
-    print(patient.married)
-    print(patient.allergies)
-    print(patient.contact)
+    print(f"Name: {patient.name}")
+    print(f"Age: {patient.age}")
+    print(f"Email: {patient.email}")
+    print(f"Any URL: {patient.fb_url}")
+    print(f"Weight: {patient.weight}")
+    print(f"Material Status: {patient.married}")
+    print(f"Allergies: {patient.allergies}")
+    print(f"Contact: {patient.contact}")
     print("Data Inserted Successfully")
 
 
