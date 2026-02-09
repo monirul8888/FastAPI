ae6857e2ed015a1753d2aec9d708882e5defeb93
monirul8888 <monirul429588@gmail.com>
2026-02-09 15:11:14 +0600

    Learning Fast-API

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, AnyUrl
+
+app=FastAPI()
+
+
+class course(BaseModel):
+    name: str
+    id: int
+    url: AnyUrl
+
+
+@app.post("/course")
+def course(post: course):
+    return {"data": post}
+
+
+@app.get("/")
+def home():
+    return {"message": "Welcome To Fast-API"}
+
+
+
+@app.get("/home")
+def home():
+    return {"message": "Welcome To Home Page"}
